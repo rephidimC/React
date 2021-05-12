c14b0e53bf71e10e64f01a2bdfb2bf752354c559
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2021-05-12 04:41:14 +0100

    introduction to components

--- introduction-to-jsx-forked/2 jsx-code-challenge/src/index.js
+++ introduction-to-jsx-forked/2 jsx-code-challenge/src/index.js
@@ -1,18 +1,5 @@
-//Create a react app from scratch.
 import React from "react";
 import ReactDOM from "react-dom";
-//It should display a h1 heading.
-ReactDOM.render(
-  <div>
-    <h1>Hello World!</h1>;
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
-//It should display an unordered list (bullet points).
+import App from "./components/App.jsx";
 
-//It should contain 3 list elements.
+ReactDOM.render(<App />, document.getElementById("root"));
