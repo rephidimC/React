066cf18affa467502e85d3b09aa64f63308e5e00
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2021-05-13 10:51:07 +0100

    ES6 Import/Export Practice-components

--- introduction-to-jsx-forked/2 jsx-code-challenge/src/index.js
+++ introduction-to-jsx-forked/2 jsx-code-challenge/src/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
+// import add from "./calculator";
+import * as All from "./calculator";
+// import { add, subtract, multiply, divide } from "./calculator";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//Import the add, multiply, subtract and divide functions
+//from the calculator.js file.
+//If successful, your website should look the same as the Final.png
+
+ReactDOM.render(
+  <ul>
+    <li>{All.add(1, 2)}</li>
+    <li>{All.multiply(2, 3)}</li>
+    <li>{All.subtract(7, 2)}</li>
+    <li>{All.divide(5, 2)}</li>
+  </ul>,
+  document.getElementById("root")
+);
